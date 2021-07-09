8c42e917a7917237d63917b8eaedcdd7e4aebfbc
Kuan-Wen Chen <kev888kev@gmail.com>
2021-07-09 11:46:08 +0800

    function arg usage

--- function/keyword args and variable arg list.py
+++ function/keyword args and variable arg list.py
@@ -1,10 +1,10 @@
-def f(arg1, arg2, *arg_list):
+def f(*arg_list, arg1, arg2):
     print("arg1:", arg1)
     print("arg2:", arg2)
     for arg in arg_list:
         print("From arg_list:", arg)
 
 
-f(100, 200, 300, -1, -2)
+# f(100, 200, 300, -1, -2) # this produce errors because keyword args are not specified.
 print("\n")
-f(arg1=100, arg2=-1111)
+f(1, 2, 3, arg1=100, arg2=-1111)
