3812c187dd2cb4491bc32a0c9c27ceb086124867
Kuan-Wen Chen <kev888kev@gmail.com>
2021-07-09 11:46:23 +0800

    Coroutine basics

---
+++ concurrency/coroutine/basic_usage.py
@@ -0,0 +1,26 @@
+import asyncio
+
+# native coroutine
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+
+# native coroutin
+async def main():
+    # Execute three count() concurrently.
+    # return a list of values if any.
+    await asyncio.gather(count(), count(), count())
+
+
+if __name__ == "__main__":
+    import time
+
+    s = time.perf_counter()
+
+    # entry point for teh async program.
+    # Create an event loop and exit with closing it.
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
