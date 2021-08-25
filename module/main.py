1ee68d1eaffd2e4d14ead475f6f4de6a36bc66b5
Kevin Chen <kev888kev@gmail.com>
2021-08-25 14:40:38 +0800

    [Module] bug fix

--- module/main.py
+++ module/main.py
@@ -1,6 +1,6 @@
 from package1.foo import f1
 from package1.package2 import bar, cad
-from .. import *  # Error, because the main module resides in the root package
+# from .. import *  # Error, because the main module resides in the root package
 
 
 f1(1, 2, 3)
