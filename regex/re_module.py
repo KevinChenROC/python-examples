380e35e0993ec8e801d0b307d99bfff3ce14b4c8
Kevin Chen <kev888kev@gmail.com>
2021-08-26 09:39:25 +0800

    [Regex] Using $ with re.M

--- regex/re_module.py
+++ regex/re_module.py
@@ -1,6 +1,16 @@
+# from enum import Flag
 import re
 
-# finditer()
+my_string = '123ABC123abc\n123abc\n'
+# # mutiple line matching, starting from the last line.
+match = re.search(r"abc$", my_string, re.M)
+
+# # Single string matching
+# match = re.search(r"abc$", my_string),
+print(match)
+print()
+
+# # finditer()
 my_string = 'abc123ABC123abc'
 pattern = re.compile(r'123')  # r"123" is a raw string
 matches = pattern.finditer(my_string)
