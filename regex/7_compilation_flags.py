4775d2b4524f0dcbd1588d6ff50eaca3c1895a79
Kevin Chen <kev888kev@gmail.com>
2021-08-26 10:24:43 +0800

    [Regex] Flags

---
+++ regex/7_compilation_flags.py
@@ -0,0 +1,21 @@
+import re
+
+my_string = "Hello World"
+# pattern = re.compile(r'world', re.IGNORECASE)  # No match without I flag
+pattern = re.compile(r'(?i:world)')  # No match without I flag
+matches = pattern.finditer(my_string)
+for match in matches:
+    print(match.group(0))
+
+print()
+
+my_string = '''
+hello
+cool
+Hello
+'''
+# line starts with ...
+pattern = re.compile(r'^[a-z]', re.MULTILINE)  # No match without M flag
+matches = pattern.finditer(my_string)
+for match in matches:
+    print(match)
