ed8f7683c16798ccc3c8b78b5c36f71fb815772a
kchen <kchen@a10networks.com>
2021-09-16 05:06:30 +0000

    ORM basic uage of query

---
+++ sqlalchemy/orm-module/basic-usage.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import or_
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm.session import Session, sessionmaker
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', nickname='%s')>" % (self.name, self.fullname, self.nickname)
+
+
+Base.metadata.create_all(engine)
+session = sessionmaker(bind=engine)()
+session.add(User(name='ed', fullname='Ed Jones', nickname='edsnickname'))
+session.add(User(name='kevin', fullname='Kevin Chen', nickname='kchen'))
+session.add(User(name='Bon', fullname='Bon Bon', nickname='BB'))
+session.commit()
+
+
+# basic query usage
+query = session.query(User)
+for row in query.filter(User.name == "kevin"):
+    print(row)
+
+for row in query.filter(or_(User.name == "kevin", User.name == "Bon")):
+    print(row)
