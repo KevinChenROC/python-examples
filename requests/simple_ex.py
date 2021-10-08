3b96bb5ca8e3574453ec31bfcf4d733dd1c49f8e
kchen <kchen@a10networks.com>
2021-10-08 03:57:41 +0000

    requests example

---
+++ requests/simple_ex.py
@@ -0,0 +1,7 @@
+import requests
+
+r = requests.get('https://httpbin.org/get')
+# r = requests.put('https://httpbin.org/put', data={'key': 'value'})
+# r = requests.delete('https://httpbin.org/delete')
+# r = requests.head('https://httpbin.org/get')
+# r = requests.options('https://httpbin.org/get')
