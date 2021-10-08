1c4b2f9dda24332f73091f93b6607ce019e2169d
kchen <kchen@a10networks.com>
2021-10-08 07:29:25 +0000

    SQLAlchemy event hook

--- sqlalchemy/core-module/hello_world.py
+++ sqlalchemy/core-module/hello_world.py
@@ -3,8 +3,8 @@ from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import Session
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
-    
-    
+
+
 # "commit as you go"
 with engine.connect() as conn:
     conn.execute(text("CREATE TABLE some_table (x int, y int)"))
@@ -13,22 +13,23 @@ with engine.connect() as conn:
         [{"x": 1, "y": 1}, {"x": 2, "y": 4}]
     )
     conn.commit()
-    
+
 # without context manager
-conn = engine.connect() 
+conn = engine.connect()
 result = conn.execute(text("select x from some_table"))
 print(result.all())
 conn.close()
 
 # autocommit
 with engine.begin() as conn:
-    result=conn.execute(text("select * from some_table where x > :x"), {"x":1.5})
+    result = conn.execute(
+        text("select * from some_table where x > :x"), {"x": 1.5})
     print(result.all())
-    
+
 # ORM usage
 with Session(engine) as session:
     result = session.execute(
         text("UPDATE some_table SET y=:y WHERE x=:x"),
-        [{"x": 9, "y":11}, {"x": 13, "y": 15}]
+        [{"x": 9, "y": 11}, {"x": 13, "y": 15}]
     )
     session.commit()
